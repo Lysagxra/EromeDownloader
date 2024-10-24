2e1d4de1ffd988bd8484500fe5345bcdd728c009
Lysagxra <g0ld3nd34d@gmail.com>
2024-10-24 14:25:51 +0200

    Delete utils directory

--- utils/extract_profile_name.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Function to extract the profile name from a given URL
-extract_profile_name()
-{
-    local url="$1"
-    local profile_name="${url##*/}"
-    echo "$profile_name"
-}
