31f932b5ef11473af7ec3f144966cde6074b8e20
Lysagxra <g0ld3nd34d@gmail.com>
2024-11-05 18:52:51 +0100

    Add files via upload

--- helpers/download_utils.py
+++ helpers/download_utils.py
@@ -3,6 +3,11 @@ This module provides utilities for handling file downloads with progress
 tracking.
 """
 
+from concurrent.futures import ThreadPoolExecutor
+
+MAX_WORKERS = 5
+TASK_COLOR = 'light_cyan3'
+
 KB = 1024
 MB = 1024 * KB
 
@@ -26,7 +31,7 @@ def get_chunk_size(file_size):
         if file_size < threshold:
             return chunk_size
 
-    return 256 * KB
+    return 128 * KB
 
 def save_file_with_progress(response, download_path, task_info):
     """
@@ -37,18 +42,16 @@ def save_file_with_progress(response, download_path, task_info):
         download_path (str): The local file path where the content should be
                              saved.
 
-    Returns:
-        None: The function performs file writing and does not return a value.
-
     Raises:
         IOError: If there is an issue writing to the specified download path.
     """
     (job_progress, task, overall_progress, overall_task) = task_info
     file_size = int(response.headers.get("content-length", -1))
+    chunk_size=get_chunk_size(file_size)
     total_downloaded = 0
 
     with open(download_path, 'wb') as file:
-        for chunk in response.iter_content(chunk_size=get_chunk_size(file_size)):
+        for chunk in response.iter_content(chunk_size=chunk_size):
             if chunk:
                 file.write(chunk)
                 total_downloaded += len(chunk)
@@ -57,3 +60,54 @@ def save_file_with_progress(response, download_path, task_info):
 
     job_progress.update(task, completed=100, visible=False)
     overall_progress.advance(overall_task)
+
+def manage_running_tasks(futures, job_progress):
+    """
+    Monitors and manages the status of running tasks.
+
+    Args:
+        futures (dict): A dictionary where keys are futures representing 
+                        asynchronous tasks and values are task information 
+                        to be used for progress updates.
+        job_progress: An object responsible for updating the progress of 
+                      tasks based on their current status.
+    """
+    while futures:
+        for future in list(futures.keys()):
+            if future.running():
+                task = futures.pop(future)
+                job_progress.update(task, visible=True)
+
+def run_in_parallel(func, items, progress_info, identifier, *args):
+    """
+    Execute a function in parallel for a list of items, updating progress in a
+    job tracker.
+
+    Args:
+        func (callable): The function to be executed for each item in the
+                         `items` list.
+        items (iterable): A list of items to be processed by the `func`.
+        progress_info: A tuple containing informations about the current job.
+        identifier (str): An identifier for the overall task.
+        *args: Additional positional arguments to be passed to the `func`.
+    """
+    (job_progress, overall_progress) = progress_info
+
+    num_items = len(items)
+    futures = {}
+
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        overall_task = overall_progress.add_task(
+            f"[{TASK_COLOR}]{identifier}", total=num_items
+        )
+        for indx, item in enumerate(items):
+            task = job_progress.add_task(
+                f"[{TASK_COLOR}]File {indx + 1}/{num_items}",
+                total=100, visible=False
+            )
+            future = executor.submit(
+                func, item, *args,
+                (job_progress, task, overall_progress, overall_task)
+            )
+            futures[future] = task
+            manage_running_tasks(futures, job_progress)
