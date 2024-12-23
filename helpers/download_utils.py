098c96cfe998550f2e6078dc92d443b275d173b6
Lysagxra <g0ld3nd34d@gmail.com>
2024-12-23 13:54:30 +0100

    Add files via upload

--- helpers/download_utils.py
+++ helpers/download_utils.py
@@ -3,48 +3,12 @@ This module provides utilities for handling file downloads with progress
 tracking.
 """
 
-import re
-from urllib.parse import unquote
 from concurrent.futures import ThreadPoolExecutor
 
 MAX_WORKERS = 3
-TASK_COLOR = 'cyan'
 
 KB = 1024
 MB = 1024 * KB
-
-def remove_special_characters(input_string):
-    """
-    Removes special characters from the input string, keeping only letters,
-    numbers, underscores, dashes, and dots.
-
-    Args:
-        input_string (str): The string from which to remove special characters.
-
-    Returns:
-        str: The cleaned string containing only letters and numbers.
-    """
-    return re.sub(r'[^a-zA-Z0-9_.-]', '', input_string)
-
-def get_episode_filename(download_link):
-    """
-    Extract the file name from the provided episode download link.
-
-    Args:
-        download_link (str): The download link for the episode.
-
-    Returns:
-        str: The extracted file name, or None if the link is None or empty.
-    """
-    if download_link:
-        try:
-            filename = unquote(download_link.split('=')[-1])  # Original name
-            return remove_special_characters(filename)        # Cleaned name
-
-        except IndexError as indx_err:
-            print(f"Error while extracting the file name: {indx_err}")
-
-    return None
 
 def get_chunk_size(file_size):
     """
@@ -57,91 +21,66 @@ def get_chunk_size(file_size):
         int: The optimal chunk size in bytes.
     """
     thresholds = [
-        (50 * MB, 256 * KB),   # Less than 50 MB
-        (100 * MB, 512 * KB),  # 50 MB to 100 MB
-        (250 * MB, 2 * MB),    # 100 MB to 250 MB
+        (1 * MB, 4 * KB),     # Less than 1 MB
+        (10 * MB, 16 * KB),   # 1 MB to 10 MB
+        (100 * MB, 64 * KB),  # 10 MB to 100 MB
     ]
 
     for threshold, chunk_size in thresholds:
         if file_size < threshold:
             return chunk_size
 
-    return 4 * MB
+    return 128 * KB
 
-def save_file_with_progress(response, final_path, task_info):
+def save_file_with_progress(response, download_path, task, live_manager):
     """
-    Saves a file to the specified path while tracking and updating progress.
+    Saves the content of a response to a file while tracking and updating
+    download progress.
 
     Args:
-        response (requests.Response): The response object containing the file
-                                      content to be downloaded.
-        final_path (str): The path where the file will be saved.
-        task_info (tuple): A tuple containing progress-related objects:
-                           - job_progress: The progress tracker for the job.
-                           - task: The specific task being tracked.
-                           - overall_task: The overall task tracker.
+        response (requests.Response): The HTTP response containing the content
+                                      to be downloaded.
+        download_path (str): The local file path where the content should be
+                             saved.
+        task (int): The task ID used to track and report progress of the
+                    download.
+        live_manager (LiveManager): An object responsible for managing and
+                                    tracking the progress of live tasks.
     """
-    (job_progress, task, overall_task) = task_info
-    file_size = int(response.headers.get('content-length', -1))
-    chunk_size = get_chunk_size(file_size)
+    file_size = int(response.headers.get("content-length", -1))
+    chunk_size=get_chunk_size(file_size)
     total_downloaded = 0
 
-    with open(final_path, 'wb') as file:
+    with open(download_path, 'wb') as file:
         for chunk in response.iter_content(chunk_size=chunk_size):
             if chunk:
                 file.write(chunk)
                 total_downloaded += len(chunk)
                 progress_percentage = (total_downloaded / file_size) * 100
-                job_progress.update(task, completed=progress_percentage)
+                live_manager.update_task(task, completed=progress_percentage)
 
-    job_progress.update(task, completed=100, visible=False)
-    job_progress.advance(overall_task)
-
-def manage_running_tasks(futures, job_progress):
+def run_in_parallel(func, items, live_manager, identifier, *args):
     """
-    Manage the status of running tasks and update their progress.
+    Executes a function in parallel for a list of items, using multiple worker
+    threads.
 
     Args:
-        futures (dict): A dictionary where keys are futures representing the
-                        tasks that have been submitted for execution, and
-                        values are the associated task identifiers in the
-                        job progress tracking system.
-        job_progress: An object responsible for tracking the progress of tasks,
-                      providing methods to update and manage task visibility.
-    """
-    while futures:
-        for future in list(futures.keys()):
-            if future.running():
-                task = futures.pop(future)
-                job_progress.update(task, visible=True)
-
-def run_in_parallel(func, items, job_progress, *args):
-    """
-    Execute a function in parallel for a list of items, updating progress in a
-    job tracker.
-
-    Args:
-        func (callable): The function to be executed for each item in the
-                         `items` list.
-        items (iterable): A list of items to be processed by the `func`.
-        job_progress: An object responsible for managing and displaying the
-                      progress of tasks.
-        *args: Additional positional arguments to be passed to the `func`.
+        func (callable): The function to execute in parallel for each item. It
+                         will be passed the current item, task ID, and the live
+                         manager along with any additional arguments.
+        items (iterable): An iterable containing the items to process in
+                          parallel.
+        live_manager (LiveManager): An object responsible for managing and
+                                    tracking the progress of live tasks.
+        identifier (str): A unique identifier used to track the overall task's
+                          progress in the live manager.
+        *args: Additional arguments passed to `func` for each execution.
     """
     num_items = len(items)
-    futures = {}
 
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        overall_task = job_progress.add_task(
-            f"[{TASK_COLOR}]Progress", total=num_items, visible=True
-        )
+        live_manager.add_overall_task(identifier, num_items)
 
-        for indx, item in enumerate(items):
-            task = job_progress.add_task(
-                f"[{TASK_COLOR}]Episode {indx + 1}/{num_items}",
-                total=100, visible=False
-            )
-            task_info = (job_progress, task, overall_task)
-            future = executor.submit(func, item, *args, task_info)
-            futures[future] = task
-            manage_running_tasks(futures, job_progress)
+        for current_task, item in enumerate(items):
+            task_id = live_manager.add_task(current_task=current_task)
+            executor.submit(func, item, task_id, live_manager, *args)
