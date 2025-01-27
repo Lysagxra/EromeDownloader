2153e6a407c829c5c370d6014a6f508290d9b1c1
Lysagxra <185969723+Lysagxra@users.noreply.github.com>
2025-01-27 18:26:21 +0100

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -4,6 +4,7 @@ the main application. These utilities include functions for downloading,
 file management, URL handling, progress tracking, and more.
 
 Modules:
+    - config: Constants and settings used across the project.
     - download_utils: Functions for handling downloads.
     - erome_utils: Functions for validating and processing Erome album URLs.
     - file_utils: Utilities for managing file operations.
@@ -17,6 +18,7 @@ to be easily imported and used across different parts of the application.
 # helpers/__init__.py
 
 __all__ = [
+    "config",
     "download_utils",
     "erome_utils",
     "file_utils",
